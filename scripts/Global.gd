bf31f52a30c00dde7c732652d5a624dfda278240
BielMaxBR <gbielmaximo@gmail.com>
2025-02-02 14:50:41 -0300

    limpeza

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -2,32 +2,46 @@ extends Node
 
 var networks = {} # network_id: { machines: [], astar: Astar2D, "color": Color }
 var machines = {} # machine: network_id
-var network_list = []
-var network_id_counter = 0
-var machine_id_counter = 0
+var network_list = [] # network_id
+var machine_id_counter = 0 # machine_id
 
-func add_machine(machine, network_id = null):
+
+# Adds a machine to the global state.
+# 
+# Parameters:
+#  - machine: The machine instance to be added.
+func add_machine(machine):
 	if not machines.has(machine):
 		machine.id = machine_id_counter
 		machine_id_counter += 1
 		machine.connect("connections_changed", self, "_on_connections_changed")
-
-	if network_id:
-		add_to_network(machine, network_id)
-		return
+		machines[machine] = null
 
 	var neighbors: Array = get_neighbors(machine)
-
+	
 	if neighbors.size() > 0:
-		var network_id_neighbor = machines[neighbors[0]]
-		if not machines.has(machine):
-			add_to_network(machine, network_id_neighbor)
-		else:
-			if network_id_neighbor == machines[machine]: return
-			merge_networks(machines[machine], network_id_neighbor)
+		for neighbor in neighbors:
+			if machines[neighbor] == null: continue
+			
+			if machines[machine] == null:
+				add_to_network(machine, machines[neighbor])
+				continue
+		
+			if machines[neighbor] != machines[machine]:
+				merge_networks(machines[machine], machines[neighbor])
 	else:
 		create_network(machine)
 
+# Merges two networks identified by their IDs.
+# 
+# This function takes two network IDs as input and merges the networks
+# associated with these IDs into a single network. The specifics of how
+# the networks are merged depend on the implementation details within
+# the function.
+#
+# @param network_id1: The ID of the first network to be merged.
+# @param network_id2: The ID of the second network to be merged.
+# @return: The function does not return a value.
 func merge_networks(network_id1, network_id2):
 	var network1 = networks[network_id1]
 	var network2 = networks[network_id2]
@@ -64,6 +78,12 @@ func update_machine_colors(network_id):
 	for machine in network.machines:
 		machine.color = network.color
 
+# Creates a network for the given machine.
+# 
+# This function initializes and sets up a network for the specified machine.
+#
+# @param machine: The machine for which the network is to be created.
+# @return: void
 func create_network(machine):
 	var network_id = 0
 	while network_list.has(network_id):
@@ -76,15 +96,4 @@ func create_network(machine):
 	update_machine_colors(network_id)
 
 func _on_connections_changed(machine):
-	# print("connections changed for ", machine.name)
-	if machine.neighbors.size() > 0:
-		for neighbor in machine.neighbors:
-			if machines[neighbor]:
-				if machines[machine] == null:
-					add_machine(machine, machines[neighbor])
-				else:
-					if machines[neighbor] != machines[machine]:
-						merge_networks(machines[machine], machines[neighbor])
-	else:
 		add_machine(machine)
-	# print("fim connections changed")
